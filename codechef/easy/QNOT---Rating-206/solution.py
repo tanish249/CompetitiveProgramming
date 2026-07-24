a02e3fc0e1713a45c13dddaa36d03643b23ca04b
TanishMehta <tanishmehta66@gmail.com>
2026-07-24 19:42:39 +0530

    ✅ QNOT - Rating 206 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-07-24

--- codechef/easy/QNOT---Rating-206/solution.py
+++ codechef/easy/QNOT---Rating-206/solution.py
@@ -1,16 +1,6 @@
-a,b,c=map(int,input().split())
-if a>=2*b and  a>=2*c:
-    print("Yes")
-else:
-    print("NO")
-    
-    
-    
-    
 # cook your dish here
-N,X,Y=map(int,input().split())
-if N>=2*X and N>=2*Y:
-    print("Yes")
+N, X, Y = map(int, input().split())
+if N >= 2 * X and N >= 2 * Y:
+    print("YES")
 else:
     print("NO")
-    
