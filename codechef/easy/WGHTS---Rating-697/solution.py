d3c3524654cb43e5ad86fd730a09fe7f9cadcb68
TanishMehta <tanishmehta66@gmail.com>
2026-07-27 11:04:17 +0530

    ✅ WGHTS - Rating 697 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-07-27

--- codechef/easy/WGHTS---Rating-697/solution.py
+++ codechef/easy/WGHTS---Rating-697/solution.py
@@ -3,7 +3,7 @@ for _ in range(t):
     a,b,c,d=map(int,input().split())
     if a==b or a==c or a==d:
         print("YES")
-    elif a==b+c or a==b+d or a==c+d:
+    elif a==b+c or a==b+d or a==c+d or a==b+c+d:
         print("YES")
     else:
         print("NO")
