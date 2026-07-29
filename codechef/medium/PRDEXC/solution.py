89fa31282f9bea58a063aaadddabebe22ea038a0
TanishMehta <tanishmehta66@gmail.com>
2026-07-29 20:20:47 +0530

    ✅ PRDEXC | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-29

--- codechef/medium/PRDEXC/solution.py
+++ codechef/medium/PRDEXC/solution.py
@@ -1,6 +1,18 @@
-t=int(input())
+t = int(input())
+
 for _ in range(t):
-    count = 0 
-    a,b,c=map(int,input().split())
-    h=a*b
-    if 
+    count = 0
+    a, b, c = map(int, input().split())
+
+    h = a * b
+
+    while h < c:
+        if a > b:
+            b += 1
+        else:
+            a += 1
+
+        h = a * b
+        count += 1
+
+    print(count)
