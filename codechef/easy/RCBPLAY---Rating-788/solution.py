c439a85e40a079efcd4af2560fcb1942d78f79be
TanishMehta <tanishmehta66@gmail.com>
2026-07-30 18:32:38 +0530

    ✅ RCBPLAY - Rating 788 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-07-30

--- codechef/easy/RCBPLAY---Rating-788/solution.py
+++ codechef/easy/RCBPLAY---Rating-788/solution.py
@@ -1,9 +1,8 @@
 t=int(input())
 for _ in range(t):
     a,b,c=map(int,input().split())
-    h=abs(a-b)
-    g=c*2
-    if g>=h:
+    h=a+2*c
+    if h>=b:
         print("YES")
     else:
         print("NO")
