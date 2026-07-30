af595e83de27cbaa12e6b114756c4e796b2ab997
TanishMehta <tanishmehta66@gmail.com>
2026-07-30 15:51:48 +0530

    ✅ FLOW010 - Rating 847 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-07-30

---
+++ codechef/easy/FLOW010---Rating-847/solution.py
@@ -0,0 +1,11 @@
+t=int(input())
+for _ in range(t):
+    a=input().lower()
+    if(a=="c"):
+        print("Cruiser")
+    elif(a=="f"):
+        print("Frigate")
+    elif(a=="d"):
+        print("Destroyer")
+    elif(a=="b"):
+        print("BattleShip")
