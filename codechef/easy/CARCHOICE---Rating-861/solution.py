6ebb710d57b4d571d567c822d6a939c65d1074c8
TanishMehta <tanishmehta66@gmail.com>
2026-07-30 16:22:20 +0530

    ✅ CARCHOICE - Rating 861 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-07-30

---
+++ codechef/easy/CARCHOICE---Rating-861/solution.py
@@ -0,0 +1,11 @@
+t=int(input())
+for _ in range(t):
+    a,b,c,d=map(int,input().split())
+    h=c//a
+    g=d//b
+    if g>h:
+        print(-1)
+    elif h==g:
+        print(0)
+    elif h>g:
+        print(1)
