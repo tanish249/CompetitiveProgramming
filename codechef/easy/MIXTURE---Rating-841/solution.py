709b11c3e360f7d0f31f67d140b48a38cecc139d
TanishMehta <tanishmehta66@gmail.com>
2026-08-01 20:01:57 +0530

    ✅ MIXTURE - Rating 841 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-01

---
+++ codechef/easy/MIXTURE---Rating-841/solution.py
@@ -0,0 +1,9 @@
+t=int(input())
+for _ in range(t):
+    a,b=map(int,input().split())
+    if a>0 and b>0:
+        print("Solution")
+    elif b==0:
+        print("Solid")
+    elif a==0:
+        print("Liquid")
