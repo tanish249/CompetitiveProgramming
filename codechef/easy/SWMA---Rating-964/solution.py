5e7b7d4bde63f3ae8e8a9077d88031479b30da4d
TanishMehta <tanishmehta66@gmail.com>
2026-08-02 15:15:21 +0530

    ✅ SWMA - Rating 964 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-02

--- codechef/easy/SWMA---Rating-964/solution.py
+++ codechef/easy/SWMA---Rating-964/solution.py
@@ -3,7 +3,7 @@ for _ in range(t):
     a,b=input().split()
     h=str(a)[::-1]
     g=str(b)[::-1]
-    if h>g or a>b:
+    if h>b or a>g or a>b or h>g:
         print('YES')
     else:
         print("NO")
