4c701f61ba95d41b77e0016483187479f0223832
TanishMehta <tanishmehta66@gmail.com>
2026-08-03 13:06:57 +0530

    ✅ DDMMORMMDD - Rating 992 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-03

--- codechef/easy/DDMMORMMDD---Rating-992/solution.py
+++ codechef/easy/DDMMORMMDD---Rating-992/solution.py
@@ -3,9 +3,10 @@ for _ in range(t):
     a=input()
     h=int(a[0]+a[1])
     g=int(a[3]+a[4])
-    if h>=12 and 12>=g:
-        print("DD/MM/YYYY")
-    elif 1<=g<=12 and 1<=h<=12:
+    if 1<=g<=12 and 1<=h<=12:
         print("BOTH")
-    else :
+    elif 1<=h<=12:
         print("MM/DD/YYYY")
+    else:
+        print("DD/MM/YYYY")
+ 
