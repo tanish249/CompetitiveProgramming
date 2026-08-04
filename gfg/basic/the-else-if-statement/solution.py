7050ce8fa7438c6517b07ff6138c4af38fbdf37e
TanishMehta <tanishmehta66@gmail.com>
2026-08-04 18:33:38 +0530

    ✅ The Else if Statement | Basic | Python | Runtime: N/A | Memory: N/A | 2026-08-04

---
+++ gfg/basic/the-else-if-statement/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def utility(self, a):
+        if a>100:
+            print("Big")
+        elif 10>a:
+            print("Small")
+        else:
+            print("Number")
+            
