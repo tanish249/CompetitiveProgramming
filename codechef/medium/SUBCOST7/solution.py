bb06c73fadd152dedcbb678829839ac54de2dccb
TanishMehta <tanishmehta66@gmail.com>
2026-08-05 20:10:18 +0530

    ✅ SUBCOST7 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-05

--- codechef/medium/SUBCOST7/solution.py
+++ codechef/medium/SUBCOST7/solution.py
@@ -2,9 +2,9 @@ t=int(input())
 for _ in range(t):
     a,b,c=map(int,input().split())
     h=abs(a-3)
-    g=abs(a-5)
+    g=3
     if 3>=a:
         print(a*b)
     else:
-        print(g8)
-   
+        print(h*c+3*b)
+  
