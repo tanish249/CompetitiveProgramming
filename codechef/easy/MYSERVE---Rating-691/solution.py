4959b9bcbdc7e24148f789c73ff515c10da26c5c
TanishMehta <tanishmehta66@gmail.com>
2026-08-07 20:37:02 +0530

    ✅ MYSERVE - Rating 691 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-07

--- codechef/easy/MYSERVE---Rating-691/solution.py
+++ codechef/easy/MYSERVE---Rating-691/solution.py
@@ -1,8 +1,8 @@
 t=int(input())
 for _ in range(t):
     a,b=map(int,input().split())
-    h=(a+b)+1
-    if h%3==0:
-        print("bob")
-    else:
+    h=(a+b)
+    if h%4==0 or h%4==1:
         print("alice")
+    else:
+        print("bob")
