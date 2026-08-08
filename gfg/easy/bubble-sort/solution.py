cd9346e14ccc9a9cf799aa8b9d2bb83255cecb93
TanishMehta <tanishmehta66@gmail.com>
2026-08-08 13:34:18 +0530

    ✅ Bubble Sort | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-08

---
+++ gfg/easy/bubble-sort/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def bubbleSort(self, nums):
+        n = len(nums)
+
+        for i in range(n):
+            for j in range(n - i - 1):
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
+        return nums
