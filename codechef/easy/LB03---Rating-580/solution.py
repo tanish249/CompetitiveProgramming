6f83fb5e4d0cd7db4fe5e607ac19824d2ab878d6
TanishMehta <tanishmehta66@gmail.com>
2026-08-10 13:45:18 +0530

    ✅ LB03 - Rating 580 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-10

---
+++ codechef/easy/LB03---Rating-580/solution.py
@@ -0,0 +1,17 @@
+#Update the '_' below to solve the problem
+
+x= 30
+if x%3== 0:
+    print('Number of questions incorrectly solved is 0')
+elif x+1%3== 0:
+    print('Number of questions incorrectly solved is 1')
+else:
+    print('Number of questions incorrectly solved is 2')
+    
+X = 34
+if x%3== 0:
+    print('Number of questions incorrectly solved is 0')
+elif x+1%3== 0:
+    print('Number of questions incorrectly solved is 1')
+else:
+    print('Number of questions incorrectly solved is 2')
