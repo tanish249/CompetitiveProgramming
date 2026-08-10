a2630193f060fc07befb83d3b1d2f6431130b7e8
TanishMehta <tanishmehta66@gmail.com>
2026-08-10 13:43:38 +0530

    ✅ LB03B - Rating 580 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-10

---
+++ codechef/easy/LB03B---Rating-580/solution.py
@@ -0,0 +1,13 @@
+# Update the program below to solve the problem
+
+t = int(input())            
+for i in range(t):          
+    a,b,c= map(int, input().split())
+    if c%a==0 and c%b==0:
+        print("ANY")
+    elif c%a==0:
+        print("CHICKEN")
+    elif c%b==0:
+        print("DUCK")
+    else:
+        print("NONE")
