7db4612d2d088503bffd813444abb2bb62758256
TanishMehta <tanishmehta66@gmail.com>
2026-08-10 13:41:34 +0530

    ✅ LB02 - Rating 591 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-10

--- codechef/easy/LB02---Rating-591/solution.py
+++ codechef/easy/LB02---Rating-591/solution.py
@@ -5,7 +5,9 @@ for i in range(t):
     a,b,c= map(int, input().split())
     if c%a==0 and c%b==0:
         print("ANY")
-    elif c%a==0 and c:
+    elif c%a==0:
         print("CHICKEN")
+    elif c%b==0:
+        print("DUCKS")
     else:
         print("NONE")
