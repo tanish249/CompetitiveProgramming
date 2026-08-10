cda92f765af76b9af76ef1878fe05117746a52a0
TanishMehta <tanishmehta66@gmail.com>
2026-08-10 13:50:23 +0530

    ✅ CS03A - Rating 276 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-10

---
+++ codechef/easy/CS03A---Rating-276/solution.py
@@ -0,0 +1,9 @@
+# Update the code below to solve the problem
+
+t = int(input())           
+for i in range(t):
+    X = int(input())
+    if X>100:
+        print(X-10)
+    else:
+        print(X)
