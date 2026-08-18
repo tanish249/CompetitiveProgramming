920a7ddeb0f6b8da1f4874960adfa5389737bbc8
TanishMehta <tanishmehta66@gmail.com>
2026-08-18 11:37:54 +0530

    ✅ WINNERR - Rating 626 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ codechef/easy/WINNERR---Rating-626/solution.py
@@ -0,0 +1,9 @@
+t=int(input())
+for _ in range(t):
+    a,b,c,d=map(int,input().split())
+    if (a==d or b==c):
+        print("TIE")
+    elif(b<d):
+        print("P")
+    elif(b>d):
+        print("Q")
