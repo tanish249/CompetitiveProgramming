83f8d7a3157c0587d7d60d4e162357aded5908fd
TanishMehta <tanishmehta66@gmail.com>
2026-08-20 22:10:37 +0530

    ✅ BETDEAL - Rating 584 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-20

--- codechef/easy/BETDEAL---Rating-584/solution.py
+++ codechef/easy/BETDEAL---Rating-584/solution.py
@@ -2,11 +2,11 @@ t=int(input())
 for _ in range(t):
     a,b=map(int,input().split())
     h=abs(100-a)
-    g=200*b//100
-    l=200-g
-    if l>h:
-        print("first")
-    elif h>l:
-        print('second')
-    else:
-        print("both")
+    g=int(200*(b/100))
+    p=abs(200-g)
+    if h==p:
+        print('BOTH')
+    elif p>h:
+        print("FIRST")
+    elif h>p:
+        print("SECOND")
