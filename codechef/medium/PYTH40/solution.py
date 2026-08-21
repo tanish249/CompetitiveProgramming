ca8da93d83debf2aa02e4c3b23ceb94369452fb8
TanishMehta <tanishmehta66@gmail.com>
2026-08-21 23:08:25 +0530

    ✅ PYTH40 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-21

--- codechef/medium/PYTH40/solution.py
+++ codechef/medium/PYTH40/solution.py
@@ -1,7 +1,2 @@
-# We need to output 2569 using string concatenation.
-# The code below is incorrect - Debug the code to solve the problem
-
-string1 = "25"
-string2 = "69"
-
-print(string1 + string2)
+text= "NumeroTres"
+print(len(text))
