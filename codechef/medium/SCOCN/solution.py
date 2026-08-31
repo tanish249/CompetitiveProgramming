8cb6eba35ac741645fcc306a3093f040bacf6e86
TanishMehta <tanishmehta66@gmail.com>
2026-08-31 20:53:08 +0530

    ✅ SCOCN | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-31

--- codechef/medium/SCOCN/solution.py
+++ codechef/medium/SCOCN/solution.py
@@ -2,7 +2,7 @@ a=int(input())
 nums=list(map(int,input().split()))
 b=int(input())
 o=nums.count(b)
-if o==1 and n in nums:
+if o==1 and b in nums:
     print(-2)
 elif b in nums:
     print(len(nums) - 1 - nums[::-1].index(b))
