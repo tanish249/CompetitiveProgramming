7be1765fb1d5de3632a7fc753c95eb8e1edce246
TanishMehta <tanishmehta66@gmail.com>
2026-08-31 21:03:08 +0530

    ✅ SCOCN | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-31

--- codechef/medium/SCOCN/solution.py
+++ codechef/medium/SCOCN/solution.py
@@ -4,10 +4,9 @@ b=int(input())
 o=nums.count(b)
 if o==1 and b in nums:
     print(-2)
-elif b in nums and :
-    
+elif b in nums:
+    q=nums.index(b)
+    w=nums.index(b,q+1)
+    print(w)
 else:
     print(-1)
-    
-    
-    
