b57b717db05902eefdb409686540a6b217b138d5
TanishMehta <tanishmehta66@gmail.com>
2026-08-31 21:01:08 +0530

    ✅ SCOCN | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-31

--- codechef/medium/SCOCN/solution.py
+++ codechef/medium/SCOCN/solution.py
@@ -2,11 +2,11 @@ a=int(input())
 nums=list(map(int,input().split()))
 b=int(input())
 o=nums.count(b)
-q=nums.index(b)
-w=nums.index(b,q+1)
 if o==1 and b in nums:
     print(-2)
 elif b in nums:
+    q=nums.index(b)
+    w=nums.index(b,q+1)
     print(w)
 else:
     print(-1)
