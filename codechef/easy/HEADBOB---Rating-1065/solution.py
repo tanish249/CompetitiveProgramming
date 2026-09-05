22c2a880af85a3ceb91a2c58e2e66fa598183c4a
TanishMehta <tanishmehta66@gmail.com>
2026-09-05 15:24:38 +0530

    ✅ HEADBOB - Rating 1065 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-05

--- codechef/easy/HEADBOB---Rating-1065/solution.py
+++ codechef/easy/HEADBOB---Rating-1065/solution.py
@@ -4,7 +4,7 @@ for _ in range(t):
     b=input()
     if "I" in b:
         print("INDIAN")
-    elif "Y" in b and "N" in b:
+    elif "Y" in b:
         print("NOT INDIAN")
     else:
         print("NOT SURE")
