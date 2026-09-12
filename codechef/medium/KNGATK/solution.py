8606982ca238dd9672abd27ff006e3349351ea3f
TanishMehta <tanishmehta66@gmail.com>
2026-09-12 13:15:30 +0530

    ✅ KNGATK | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-12

--- codechef/medium/KNGATK/solution.py
+++ codechef/medium/KNGATK/solution.py
@@ -2,10 +2,5 @@ t=int(input())
 for _ in range(t):
     a=int(input())
     nums=list(map(int,input().split()))
-    
-    n=len(nums)
-    for i in range(n):
-        for j in range(n-1-i):
-            if nums[j] > nums[j+1]:
-               nums[j] , nums[j+1] = nums[j+1] , nums[j]
+    nums.sort()
     print(nums[1])
