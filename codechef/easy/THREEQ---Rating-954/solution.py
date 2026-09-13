dc415f17753633c9fe2a3eb84ef444e9341e4519
TanishMehta <tanishmehta66@gmail.com>
2026-09-13 11:14:50 +0530

    ✅ THREEQ - Rating 954 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-13

---
+++ codechef/easy/THREEQ---Rating-954/solution.py
@@ -0,0 +1,12 @@
+t=int(input())
+for _ in range(t):
+    num1=list(map(int,input().split()))
+    num2=list(map(int,input().split()))
+    g=num1.count(0)
+    h=num1.count(1)
+    x=num2.count(0)
+    y=num2.count(1)
+    if g==x and h==y:
+        print("PASS")
+    else:
+        print("FAIL")
