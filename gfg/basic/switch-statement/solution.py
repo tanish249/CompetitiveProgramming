8551a5b3f38afd0a313fbcaf4b348bfbf19c0076
TanishMehta <tanishmehta66@gmail.com>
2026-09-14 10:20:43 +0530

    ✅ Switch Statement | Basic | Python | Runtime: N/A | Memory: N/A | 2026-09-14

---
+++ gfg/basic/switch-statement/solution.py
@@ -0,0 +1,21 @@
+a=int(input())
+if a==1:
+    print("One")
+elif a==2:
+    print("Two")
+elif a==3:
+    print("Three")
+elif a==4:
+    print("Four")
+elif a==5:
+    print("Five")
+elif a==6:
+    print("Six")
+elif a==7:
+    print("Seven")
+elif a==8:
+    print("Eight")
+elif a==9:
+    print("Nine")
+else:
+    print("Unknown")
