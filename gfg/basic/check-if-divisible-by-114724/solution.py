eb58d7bb3865382cc3f1cdada43ab64f4fc0460a
TanishMehta <tanishmehta66@gmail.com>
2026-09-19 19:24:33 +0530

    ✅ Check if divisible by 11 | Basic | Python | Runtime: N/A | Memory: N/A | 2026-09-19

---
+++ gfg/basic/check-if-divisible-by-114724/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def divisibleBy11(self, s: str) -> bool:
+        s=int(s)
+        if s%11==0:
+            return True
+        else:
+            return False
+       
+      
