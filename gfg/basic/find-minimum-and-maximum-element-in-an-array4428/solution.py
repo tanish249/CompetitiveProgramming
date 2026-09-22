9bc1ec45c59e5cdf62136fc9cfdc49e30e076d78
TanishMehta <tanishmehta66@gmail.com>
2026-09-22 22:47:49 +0530

    ✅ Min and Max in Array | Basic | Python | Runtime: N/A | Memory: N/A | 2026-09-22

--- gfg/basic/find-minimum-and-maximum-element-in-an-array4428/solution.py
+++ gfg/basic/find-minimum-and-maximum-element-in-an-array4428/solution.py
@@ -1,5 +1,35 @@
 class Solution:
     def getMinMax(self, arr):
-        h = min(arr)
-        g = max(arr)
-        return h, g
+        def merge_sort(arr):
+
+            if len(arr) <= 1:
+                return arr
+
+            mid = len(arr) // 2
+            left = merge_sort(arr[:mid])
+            right = merge_sort(arr[mid:])
+
+            result = []
+            i = 0
+            j = 0
+
+            while i < len(left) and j < len(right):
+                if left[i] < right[j]:
+                    result.append(left[i])
+                    i += 1
+                else:
+                    result.append(right[j])
+                    j += 1
+
+            while i < len(left):
+                result.append(left[i])
+                i += 1
+
+            while j < len(right):
+                result.append(right[j])
+                j += 1
+
+            return result
+
+        arr = merge_sort(arr)
+        return arr[0], arr[-1]
