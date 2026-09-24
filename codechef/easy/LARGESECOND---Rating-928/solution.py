de43620c0fe5bcbe911ad0220f6e9d345ec9e78f
TanishMehta <tanishmehta66@gmail.com>
2026-09-24 21:51:14 +0530

    ✅ LARGESECOND - Rating 928 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-24

--- codechef/easy/LARGESECOND---Rating-928/solution.py
+++ codechef/easy/LARGESECOND---Rating-928/solution.py
@@ -2,11 +2,37 @@ t = int(input())
 
 for _ in range(t):
     a = int(input())
-    nums = list(set(map(int, input().split())))
+    arr = list(set(map(int, input().split())))
 
-    nums.sort()
-
-    h = nums[-1]
-    g = nums[-2]
-
-    print(h + g)
+    def merge_sort(arr):
+        if len(arr)<=1:
+            return arr
+        mid=len(arr)//2
+        left=merge_sort(arr[:mid])
+        right=merge_sort(arr[mid:])
+        
+        result =[]
+        i = 0
+        j= 0
+        while i<len(left) and j<len(right):
+            if left[i]<right[j]:
+                result.append(left[i])
+                i +=1
+            else:
+                result.append(right[j])
+                j +=1
+        while i<len(left):
+            result.append(left[i])
+            i +=1
+        while j<len(right):
+            result.append(right[j])
+            j +=1
+        return result 
+        
+    result=merge_sort(arr)
+    
+    h=result[-1]
+    g=result[-2]
+    print(h+g)
+            
+        
