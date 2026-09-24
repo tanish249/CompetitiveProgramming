c68e49df837f2514cf44303b87ac76cd19914e00
TanishMehta <tanishmehta66@gmail.com>
2026-09-24 22:02:53 +0530

    ✅ SORTARRAYS | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-24

--- codechef/medium/SORTARRAYS/solution.py
+++ codechef/medium/SORTARRAYS/solution.py
@@ -1,3 +1,35 @@
 def sort_colors(nums):
-    nums.sort()
-    return nums
+
+    def merge_sort(nums):
+        if len(nums) <= 1:
+            return nums
+
+        mid = len(nums) // 2
+
+        left = merge_sort(nums[:mid])
+        right = merge_sort(nums[mid:])
+
+        result = []
+
+        i = 0
+        j = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+
+        while i < len(left):
+            result.append(left[i])
+            i += 1
+
+        while j < len(right):
+            result.append(right[j])
+            j += 1
+
+        return result
+
+    return merge_sort(nums)
